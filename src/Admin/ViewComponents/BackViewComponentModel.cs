8ace672075233db1a5a40bf9ea950f16b366ff93
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2021-04-18 10:58:46 -0300

    chore(admin): Removed unused BackViewComponent

--- src/Admin/ViewComponents/BackViewComponentModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Nocturne.Auth.Admin.ViewComponents
-{
-    public class BackViewComponentModel
-    {
-        public string Url { get; set; }
-
-        public string Title { get; set; }
-    }
-}
