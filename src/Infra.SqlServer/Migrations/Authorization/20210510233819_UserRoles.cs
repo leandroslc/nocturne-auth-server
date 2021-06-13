b1dac80151c329fe5f46192a92281342ca46cf37
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2021-06-13 17:08:16 -0300

    chore(infra): Included identity and authorization migrations

---
+++ src/Infra.SqlServer/Migrations/Authorization/20210510233819_UserRoles.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationsAuthorization
+{
+    public partial class UserRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                schema: "auth",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    RoleId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "auth",
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                schema: "auth",
+                table: "UserRoles",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRoles",
+                schema: "auth");
+        }
+    }
+}
---
+++ src/Infra.SqlServer/Migrations/Identity/20210508233359_AddUserNameAndEnabled.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations.Identity
+{
+    public partial class AddUserNameAndEnabled : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                schema: "identity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                schema: "identity",
+                table: "Users",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                schema: "identity",
+                table: "Users");
+        }
+    }
+}
