2217a4b6631c69a5d12afd9aa4c96c1f3e9be704
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2021-07-13 20:02:51 -0300

    improvement(server): Added route names for /connect/logout and /connect/authorize

--- src/Server/Configuration/Constants/RouteNames.cs
+++ src/Server/Configuration/Constants/RouteNames.cs
@@ -2,6 +2,10 @@ namespace Nocturne.Auth.Server.Configuration.Constants
 {
     public static class RouteNames
     {
+        public const string AuthorizationAuthorize = nameof(AuthorizationAuthorize);
+
+        public const string AuthorizationLogout = nameof(AuthorizationLogout);
+
         public const string Home = nameof(Home);
     }
 }
